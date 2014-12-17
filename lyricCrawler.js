726bd0089df12f9bf29480513a5683a905aa2f12
root <root@centos-1.(none)>
2014-12-17 22:26:06 +0800

    update:1.read url from a file;2.rename lrc to number+_+chinese.

--- lyricCrawler.js
+++ lyricCrawler.js
@@ -7,7 +7,7 @@ var request = require('request');
 
 var mainUrl = 'http://music.baidu.com/';
 
-var urls = [];
+//var urls = [];
 var baiduMusicUrl = 'http://music.baidu.com/album/73078';//恋恋风尘
 var baiduMusicUrl1 = 'http://music.baidu.com/album/123890520';//在路上
 var baiduMusicUrl2 = 'http://music.baidu.com/album/7288115';//北京的冬天
@@ -18,13 +18,17 @@ var baiduMusicUrl6 = 'http://music.baidu.com/album/165293';//单曲-睡在我上
 
 //var baiduMusicUrl = 'http://music.baidu.com/album/7288187';//生如夏花
 
-urls.push(baiduMusicUrl);
-urls.push(baiduMusicUrl1);
-urls.push(baiduMusicUrl2);
-urls.push(baiduMusicUrl3);
-urls.push(baiduMusicUrl4);
-urls.push(baiduMusicUrl5);
-urls.push(baiduMusicUrl6);
+//urls.push(baiduMusicUrl);
+//urls.push(baiduMusicUrl1);
+//urls.push(baiduMusicUrl2);
+//urls.push(baiduMusicUrl3);
+//urls.push(baiduMusicUrl4);
+//urls.push(baiduMusicUrl5);
+//urls.push(baiduMusicUrl6);
+
+var urls = fs.readFileSync('./musicUrlList.txt', 'utf8').split(/\r?\n/ig);
+
+console.log(urls);
 
 if (!fs.existsSync('./lyric')) {
   fs.mkdirSync('./lyric');
@@ -32,6 +36,7 @@ if (!fs.existsSync('./lyric')) {
 
 for(var i in urls){
   var baiduMusicUrl = urls[i];
+	if(baiduMusicUrl.trim() === '') continue;
 	superagent.get(baiduMusicUrl).end(function(err, res){
 		 if(err) {
 				console.log(err);
@@ -56,7 +61,7 @@ for(var i in urls){
 					 var $ = cheerio.load(res.text);
 					 
 					 var file_url = urlhelp.resolve(mainUrl,JSON.parse($('.down-lrc-btn').first().attr('data-lyricdata')).href);
-					 var file_name = $('.name').first().text().trim() + '.lrc';
+					 var file_name = file_url.split('/').pop().split('.')[0] + '_' + $('.name').first().text().trim() + '.lrc';
 					 
 					 if(file_url != mainUrl) {//防止下载LRC歌词的链接为空
 							request.head(file_url, function(err, res, body){
